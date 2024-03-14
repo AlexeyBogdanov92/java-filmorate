f0dd9c7bb105a39d624c9eb155200159087a891a
AlexeyBogdanov92 <lexx_7@list.ru>
2024-03-15 02:13:15 +0700

    задания по ТЗ10

--- src/main/java/ru/yandex/practicum/filmorate/model/ErrorResponse.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/ErrorResponse.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ErrorResponse {
     private final String error;
+
     public String getError() {
         return error;
     }
